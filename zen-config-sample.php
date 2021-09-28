942eb43537743c1b419186e87bfbb573b4ef9bcd
Daniel Naval <danielnaval2702@gmail.com>
2021-09-28 07:15:50 -0400

    Delete zen-config-sample.php

--- zen-config-sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-// ** MYSQL Database Credentials - DNAVAL ** //
-define( 'DAN_DB_HOST', 'localhost' );
-define( 'DAN_DB_NAME', 'dbname' );
-define( 'DAN_DB_USER', 'dbuser' );
-define( 'DAN_DB_PASSWORD', 'dbpwd' );
